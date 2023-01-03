c4e2a0494d1a2edfdf1153bce0da0d253d88b3b8
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2023-01-03 15:34:55 -0300

    new: component Layout

---
+++ src/assets/styles/index.jsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+   width: 1100px;
+   height: 700px;    
+   margin: auto;
+`
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { LayoutContainer } from "../../assets/styles";
+
+export default function Layout(){
+   return (
+      <LayoutContainer>
+         
+      </LayoutContainer>
+   )
+}
