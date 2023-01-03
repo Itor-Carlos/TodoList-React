7f9dff8a197a5f42e516a02ae0de0b7ee3032524
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2023-01-03 19:22:59 -0300

    add some cards to test the CardList component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import { LayoutContainer } from "../../assets/styles";
+import Card from "../Card";
+import CardList from "../CardList";
 import Header from "../Header";
 
 export default function Layout(){
+   const cards = [
+      {
+         name: "Exercise List - Algebra Linear",
+         description: 'List of Linear Algebra subject exercises',
+         todoStatus: 'CONCLUIDO'
+      },
+      {
+         name: "Exercise List - Funcional Programming",
+         description: 'List of Functional Programming subject exercises',
+         todoStatus: 'PENDENTE'
+      },
+      {
+         name: "Exercise List - Calculus II",
+         description: 'List of Functional Programming subject exercises',
+         todoStatus: 'CONCLUIDO'
+      },
+      {
+         name: "Exercise List - Phists",
+         description: 'List of Phsics subject exercises',
+         todoStatus: 'CONCLUIDO'
+      }
+   ]
+   
    return (
       <LayoutContainer>
          <Header/>
+         <CardList cardLists={cards}>
+            
+         </CardList>
       </LayoutContainer>
    )
 }
