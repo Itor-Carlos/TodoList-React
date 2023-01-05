ff21fde2a672352909f3e0c173cd7882eae00408
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2023-01-05 18:43:30 -0300

    fix: infinity loop without useCallback hook

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import CardList from "../../components/CardList";
 import Header from "../../components/Header";
 import api from '../../api/api'
@@ -7,17 +7,24 @@ export default function Home(){
 
    const [cards, setCards] = useState([]);
 
-   const fetchCards = async () => {
-      await api.get('/todos')
-               .then((response) => setCards(response.data))
-               .catch((err) => {
-                  console.log(err)
-               })
-   }
+   const fetchCards = useCallback(async () => {
+      try{
+         const resposta = await api.get('/todos')
+         if(resposta.status === 200){
+            setCards(resposta.data)
+         }
+         else{
+            alert(resposta.data)
+         }
+      }
+      catch(error){
+         alert(error)
+      }
+   },[])
    
    useEffect(() => {
       fetchCards();
-   }, [])
+   }, [fetchCards])
 
    return (
       <>
